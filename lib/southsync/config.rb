bcdc289a1bb43a21bb8b1de3c206e349a14a5ca9
miniblk <miniblk@proton.me>
2025-01-17 02:24:44 +0700

    refactor: change method name and loading mechanism for load_folder with different spinner, remove redundant code, add all_files method to list files

--- lib/southsync/config.rb
+++ lib/southsync/config.rb
@@ -5,55 +5,58 @@ module SouthSync
   module Config
     ROOT_DIR = File.expand_path('../..', __dir__)
     CONFIG_FILE = File.join(ROOT_DIR, '/config/config.yml')
+    EXTENSIONS = ['.mkv', '.mp4', '.avi'].freeze
 
     def first_time?
-      true unless File.exist?(CONFIG_FILE) && load_config['show_location']
+      true unless File.exist?(CONFIG_FILE) && load_config
     end
 
-    def folder_path
-      ask('Enter folder location...').strip
-    end
-
-    def count_files(path)
-      extensions = ['.mkv', '.mp4', '.avi']
-      entries = Dir.entries(path)
-      valid_files = entries.count { |entry| extensions.include?(File.extname(entry)) }
-
-      puts "Found #{valid_files} video files / #{entries.count} files"
-    end
-
-    def load_folder
-      full_path = File.expand_path(load_config['show_location'])
-      loading_thread = Thread.new { loading_indicator(load_config['show_location']) }
-      loading_thread.join(2)
-
-      print Dir.exist?(full_path) ? @msg[:success] : @msg[:fail]
-      count_files(full_path)
-
-      Thread.kill(loading_thread)
-      sleep 1
-    rescue Errno::ENOENT => e
-      puts "Error while loading files...\n#{e}"
-      # still need to handle this later... should i ensure to rm config.yml?
+    def base_dir
+      load_config['show_location']
     end
 
     def make_config
+      folder_path = CLI::Input.ask('Enter folder location...').strip
+      exit unless valid_folder?(folder_path)
+
       config = { 'show_location' => folder_path }
       FileUtils.mkdir_p("#{ROOT_DIR}/config")
-      config_file = File.open(CONFIG_FILE, 'w+')
-      config_file.write(config.to_yaml)
+      File.open(CONFIG_FILE, 'w+') do |file|
+        file.write(config.to_yaml)
+      end
     rescue IOError => e
-      puts "Error reading file: #{e.message}"
-    ensure
-      config_file&.close
+      puts "\n[!] Error reading file: #{e.message}"
     end
 
     def load_config
       YAML.safe_load(File.read(CONFIG_FILE), permitted_classes: [Symbol])
     rescue Errno::ENOENT => e
-      puts "Error loading config file: #{e.message}"
+      puts "\n[!] Error while loading config file: #{e.message}"
     rescue Psych::SyntaxError, Psych::DisallowedClass => e
       puts "YAML parsing error: #{e.message}"
+    end
+
+    def valid_folder?(path = base_dir)
+      files_range = all_files(path).map(&:size)
+      CLI::Display.spin(files_range)
+      print "...kewl!\n"
+      sleep 1
+      true
+    rescue Interrupt, Errno::ENOENT, Errno::ENOTDIR => e
+      puts "\n[Lame!] Error while loading files...\n#{e}"
+      false
+    end
+
+    def all_files(path = base_dir)
+      full_path = File.expand_path(path)
+      entries = Dir.entries(full_path) - ['.', '..']
+      files = entries.select { |entry| File.file?(File.join(full_path, entry)) }
+
+      video_files = files.select { |entry| EXTENSIONS.include?(File.extname(entry)) }
+      [video_files, files]
+    rescue Errno::ENOENT => e
+      puts "[!] #{e.message}"
+      exit
     end
   end
 end
